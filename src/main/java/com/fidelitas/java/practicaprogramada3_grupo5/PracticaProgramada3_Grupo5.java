6e462a637ff6ee213086d34d2a2b47a9a8cff227
David Vargas Arias <vargasariasdavid110@gmail.com>
2026-06-28 19:12:16 -0600

    first commit

---
+++ src/main/java/com/fidelitas/java/practicaprogramada3_grupo5/PracticaProgramada3_Grupo5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.fidelitas.java.practicaprogramada3_grupo5;
+
+/**
+ *
+ * @author varga
+ */
+public class PracticaProgramada3_Grupo5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
