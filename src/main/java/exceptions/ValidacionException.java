d883ad85ca1770fdcd9d5361e6590fc8820bae5b
David Vargas Arias <vargasariasdavid110@gmail.com>
2026-07-06 14:12:18 -0600

    second commit

---
+++ src/main/java/exceptions/ValidacionException.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exceptions;
+
+/**
+ *
+ * @author varga
+ */
+public class ValidacionException extends Exception {
+
+    //Constructor de la clase para las exepciones
+    public ValidacionException(String message) {
+        super(message);
+    }
+    
+}
---
+++ src/main/java/model/CursoEstudiante.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author varga
+ */
+public enum CursoEstudiante {
+    PROGRAMACION,
+    BASES_DATOS,
+    REDES,
+    IA,
+    DISENO_SISTEMAS;
+
+    @Override
+    public String toString() {
+        return name();
+    }    
+}
